a25c7eda762a89d6108bc1fc5e72b0db57ace88a
IbnuJabir <kedirjabir12@gmail.com>
2023-06-15 13:59:26 -0700

    k

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,8 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 
-/**
- * main - Entry point
- * Return: Always 1 (Success)
- */
-int main(void)
+int main()
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+    char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    write(2, message, sizeof(message) - 1);
+    return 1;
 }
