d30c3f01f98ef3b663152a62eb330f4ea66cac52
IbnuJabir <kedirjabir12@gmail.com>
2023-06-20 03:58:19 -0700

    k

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the english alphabet from a-z.
+ * Return: Nothing.
+ */
+void print_alphabet(void)
+{
+	char k;
+
+	for (k = 'a'; k <= 'z'; k++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
