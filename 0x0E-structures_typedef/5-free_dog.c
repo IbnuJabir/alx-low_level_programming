482ab20ddad4f5be31fd970b577b257fbb8410d8
IbnuJabir <kedirjabir12@gmail.com>
2023-07-17 03:15:35 -0700

    k_now

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -15,21 +15,4 @@ void free_dog(dog_t *d)
 		free(d->owner);
 		free(d);
 	}
-}#include <stdlib.h>
-#include "dog.h"
-
-/**
- * free_dog - frees dogs
- * @d: pointer to dog to free
- *
- * Return: void
- */
-void free_dog(dog_t *d)
-{
-	if (d)
-	{
-		free(d->name);
-		free(d->owner);
-		free(d);
-	}
 }
