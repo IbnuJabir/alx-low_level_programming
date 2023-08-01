187937bf23bdf97f01be212e35f818abaa28a396
IbnuJabir <kedirjabir12@gmail.com>
2023-08-01 07:06:01 -0700

    kjssa

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -30,7 +30,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (prev);
 	}
 	nxt = *head;
-	nxt = (*(head))->next;
+	str = (*(head))->next;
 	while (str != NULL)
 	{
 		if (k == idx)
