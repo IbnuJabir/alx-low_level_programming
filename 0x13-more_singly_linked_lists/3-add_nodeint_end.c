5e3d6eb7784462aa114cc8ab392c67161dcc3995
IbnuJabir <kedirjabir12@gmail.com>
2023-08-01 00:42:55 -0700

    kjs

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -9,26 +9,26 @@
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-    listint_t *new, *last;
+	listint_t *new, *last;
 
-    new = malloc(sizeof(listint_t));
-    if (new == NULL)
-        return (NULL);
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+	return (NULL);
 
-    new->n = n;
-    new->next = NULL;
+	new->n = n;
+	new->next = NULL;
 
-    if (*head == NULL)
-    {
-        *head = new;
-        return (new);
-    }
+	if (*head == NULL)
+	{
+	*head = new;
+	return (new);
+	}
 
-    last = *head;
-    while (last->next != NULL)
-        last = last->next;
+	last = *head;
+	while (last->next != NULL)
+		last = last->next;
 
-    last->next = new;
+	last->next = new;
 
-    return (new);
+	return (new);
 }
