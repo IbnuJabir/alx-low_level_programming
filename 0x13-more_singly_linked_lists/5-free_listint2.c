c8a17b05a833fd848ad890d685b25ec9f344121c
IbnuJabir <kedirjabir12@gmail.com>
2023-08-01 02:25:31 -0700

    kjfs

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,18 +10,15 @@
 
 void free_listint2(listint_t **head)
 {
-	if (head == NULL)
+	listint_t *temp;
+
+	if (head == NULL || *head == NULL)
 		return;
-
-	listint_t *current = *head;
-	listint_t *next = NULL;
-
-	while (current != NULL)
+	while (*head != NULL)
 	{
-		next = current->next;
-		free(current);
-		current = next;
+		temp = *head;
+		*head = (*(head))->next;
+		free(temp);
 	}
-
 	*head = NULL;
 }
