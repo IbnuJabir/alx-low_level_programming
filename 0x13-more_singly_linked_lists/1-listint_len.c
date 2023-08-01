79bfae8ae98f5610f3b06805da2ed60cb118d1b9
IbnuJabir <kedirjabir12@gmail.com>
2023-07-31 23:32:28 -0700

    kjs$

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -8,13 +8,13 @@
  */
 size_t listint_len(const listint_t *h)
 {
-    size_t i = 0;
+	size_t i = 0;
 
-    while( h != NULL)
-    {
-        h = h->next;
+	while (h != NULL)
+	{
+	h = h->next;
 	i++;
-    }
+	}
 
-    return (i);
+	return (i);
 }
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,15 +1,19 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stddef.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * 
+ *
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
@@ -27,3 +31,5 @@ size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 int _putchar(char c);
+
+#endif /*LISTS_H*/
