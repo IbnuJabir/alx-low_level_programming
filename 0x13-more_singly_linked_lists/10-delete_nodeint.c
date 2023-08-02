57045c842e52619b1ed4000b5cfccc25e26d563d
IbnuJabir <kedirjabir12@gmail.com>
2023-08-02 01:56:12 -0700

    jkl

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -19,7 +19,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (-1);
 	if (index == 0)
 	{
-		next = (*head)->next;
+		ptr = (*head)->next;
 		free(*head);
 		*head = ptr;
 		return (1);
