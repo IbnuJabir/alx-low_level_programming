71e1b9fb5fccfc3891699826ea6908214b1850a5
IbnuJabir <kedirjabir12@gmail.com>
2023-08-02 02:12:28 -0700

    kjl

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,22 @@
+/**
+ * reverse_listint - Reverses a listint_t linked list in place.
+ * @head: A pointer to a pointer to the head node of the list.
+ *
+ * Return: A pointer to the first node of the reversed list.
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *prev = NULL;
+	listint_t *curr = *head;
+
+	while (curr != NULL)
+	{
+		listint_t *next = curr->next;
+		curr->next = prev;
+		prev = curr;
+		curr = next;
+	}
+
+	*head = prev;
+	return *head;
+}
