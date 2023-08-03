a2f21ab8db6629557c273df6f6bab141637e4773
IbnuJabir <kedirjabir12@gmail.com>
2023-08-03 06:00:13 -0700

    k

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -5,31 +5,28 @@
  * print_binary - Prints the binary representation of a number.
  * @n: The number to print in binary.
  */
+
 void print_binary(unsigned long int n)
 {
+	unsigned int srt, lim;
+
+	srt = 0;
+	lim = 32768;
+
 	if (n == 0)
 	{
 		_putchar('0');
 		return;
 	}
-
-	unsigned long int mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
-
-	while ((mask & n) == 0)
+	while (lim)
 	{
-		mask >>= 1;
-	}
-
-	while (mask != 0)
-	{
-		if ((mask & n) != 0)
+		if (srt == 1 && (n & lim) == 0)
+			_putchar('0');
+		else if ((n & lim) != 0)
 		{
 			_putchar('1');
-		} else
-		{
-			_putchar('0');
+			srt = 1;
 		}
-
-		mask >>= 1;
+		lim >>= 1;
 	}
 }
