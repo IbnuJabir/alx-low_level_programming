6dcd8a1eefbbcf25427d79dcbbc55c4f1c1ad948
IbnuJabir <kedirjabir12@gmail.com>
2023-09-13 06:37:39 -0700

    kjs

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,13 +1,15 @@
 #include "function_pointers.h"
 
 /**
- * print_name - say hello to the given name
- * @name: name of the person
- * @f: pointer to a function
+ * print_name - a function that prints a name.
+ * @name: the name given
+ * @f: function of name
  *
- * Return: nothing
+ * Rteurn: void
  */
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if (name != NULL && f != NULL)
+
+		f(name);
 }
